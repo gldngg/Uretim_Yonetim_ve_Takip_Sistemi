7cad9c1266fb97a35f7365a329fbfd91d4dd4a06
gldngg <gldngngr.0@gmail.com>
2026-04-27 23:41:51 +0300

    Update OperatorMenu.java

--- src/üys/OperatorMenu.java
+++ src/üys/OperatorMenu.java
@@ -3,118 +3,148 @@ package üys;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
 
-public class OperatorMenu extends JFrame{
-	
-	JPanel contentPane = new JPanel();
-	
+import database.Session;
+
+public class OperatorMenu extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
 	public OperatorMenu() {
-				//ana ekran
-				setTitle("Operatör Menü");
-				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				setBounds(100, 100, 900, 600);
-				contentPane.setBackground(new Color(142, 155, 213));
-				contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-				setContentPane(contentPane);
-				contentPane.setLayout(null);
-				
-				//üst mavi panel
-				JPanel headerPanel = new JPanel();
-				headerPanel.setBackground(new Color(63, 81, 181));
-				headerPanel.setBounds(0, 0, 900, 60);
-				contentPane.add(headerPanel);
-				headerPanel.setLayout(null);
-				
-				JLabel lblTitle = new JLabel("Hoşgeldiniz, [Operatör]");
-				lblTitle.setBackground(new Color(255, 255, 255));
-				lblTitle.setBounds(0, 11, 900, 40);
-				lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 20));
-				lblTitle.setForeground(new Color(255, 255, 255));
-				lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
-				headerPanel.add(lblTitle);
-				
-				ImageIcon raporIcon = new ImageIcon("RaporlarIcon.png");//button 1
-				Image image1 = raporIcon.getImage();
-				Image newimg1 = image1.getScaledInstance(125, 100,  java.awt.Image.SCALE_SMOOTH); 
-				raporIcon = new ImageIcon(newimg1);
-				JButton btnRapor = new JButton(raporIcon);
-				btnRapor.setFont(new Font("Tahoma", Font.BOLD, 14));
-				btnRapor.setForeground(new Color(255, 255, 255));
-				btnRapor.setText("Raporlar");
-				btnRapor.setBorderPainted(false);
-				btnRapor.setBackground(new Color(63, 81, 181));
-				btnRapor.setVerticalTextPosition(SwingConstants.BOTTOM);
-				btnRapor.setHorizontalTextPosition(SwingConstants.CENTER);
-				btnRapor.setBounds(50, 200, 175, 150);
-				contentPane.add(btnRapor);
-				/*btnRapor.addActionListener(e -> 
-				Raporlar r = new Raporlar();
-				r.setVisible(true);
-				this.dispose(););*/
-				
-				ImageIcon makineIcon = new ImageIcon("MakineGirişIcon.png");//button 2
-				Image image2 = makineIcon.getImage();
-				Image newimg2 = image2.getScaledInstance(125, 100,  java.awt.Image.SCALE_SMOOTH); 
-				makineIcon = new ImageIcon(newimg2);
-				JButton btnMakine = new JButton(makineIcon);
-				btnMakine.setVerticalTextPosition(SwingConstants.BOTTOM);
-				btnMakine.setText("Makine Girişi");
-				btnMakine.setHorizontalTextPosition(SwingConstants.CENTER);
-				btnMakine.setForeground(Color.WHITE);
-				btnMakine.setFont(new Font("Tahoma", Font.BOLD, 14));
-				btnMakine.setBorderPainted(false);
-				btnMakine.setBackground(new Color(63, 81, 181));
-				btnMakine.setBounds(255, 200, 175, 150);
-				contentPane.add(btnMakine);
-				/*btnMakine.addActionListener(e -> 
-				MakineGirisEkrani m = new MakineGirisEkrani();
-				r.setVisible(true);
-				this.dispose(););*/
-				
-				ImageIcon planlamaIcon = new ImageIcon("PlanlamaIcon.png");//button 3
-				Image image3 = planlamaIcon.getImage();
-				Image newimg3 = image3.getScaledInstance(125, 100,  java.awt.Image.SCALE_SMOOTH); 
-				planlamaIcon = new ImageIcon(newimg3);
-				JButton btnPlanlama = new JButton(planlamaIcon);
-				btnPlanlama.setVerticalTextPosition(SwingConstants.BOTTOM);
-				btnPlanlama.setText("Planlama");
-				btnPlanlama.setHorizontalTextPosition(SwingConstants.CENTER);
-				btnPlanlama.setForeground(Color.WHITE);
-				btnPlanlama.setFont(new Font("Tahoma", Font.BOLD, 14));
-				btnPlanlama.setBorderPainted(false);
-				btnPlanlama.setBackground(new Color(63, 81, 181));
-				btnPlanlama.setBounds(460, 200, 175, 150);
-				contentPane.add(btnPlanlama);
-				/*btnPlanlama.addActionListener(e -> 
-				Planlama p = new Planlama();
-				p.setVisible(true);
-				this.dispose(););*/
-				
-				ImageIcon durusIcon = new ImageIcon("DurusKayıpIcon.png");//button 4
-				Image image4 = durusIcon.getImage();
-				Image newimg4 = image4.getScaledInstance(125, 100,  java.awt.Image.SCALE_SMOOTH); 
-				durusIcon = new ImageIcon(newimg4);
-				JButton btnDurusKayip = new JButton(durusIcon);
-				btnDurusKayip.setVerticalTextPosition(SwingConstants.BOTTOM);
-				btnDurusKayip.setText("<html><center>Duruş/Kayıp<br>Yönetimi</center></html>");
-				btnDurusKayip.setHorizontalTextPosition(SwingConstants.CENTER);
-				btnDurusKayip.setForeground(Color.WHITE);
-				btnDurusKayip.setFont(new Font("Tahoma", Font.BOLD, 14));
-				btnDurusKayip.setBorderPainted(false);
-				btnDurusKayip.setBackground(new Color(63, 81, 181));
-				btnDurusKayip.setBounds(665, 200, 175, 150);
-				contentPane.add(btnDurusKayip);
-				/*btnDurusKayip.addActionListener(e -> 
-				DuruşKayıp d = new DuruşKayıp();
-				d.setVisible(true);
-				this.dispose(););*/
-		
+
+		setTitle("Operatör Menü");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1100, 700);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(142, 155, 213));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JPanel headerPanel = new JPanel();
+		headerPanel.setBackground(new Color(63, 81, 181));
+		headerPanel.setBounds(0, 0, 1100, 100);
+		headerPanel.setLayout(null);
+		contentPane.add(headerPanel);
+
+		JLabel lblTitle = new JLabel("Hoşgeldiniz, " + Session.aktifKullanici + "!");
+		lblTitle.setBounds(0, 25, 1100, 40);
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblTitle.setForeground(Color.WHITE);
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		headerPanel.add(lblTitle);
+
+		JButton btnCikis = new JButton("ÇIKIŞ");
+		btnCikis.setBounds(950, 30, 100, 30);
+		headerPanel.add(btnCikis);
+
+		btnCikis.addActionListener(e -> {
+			Session.aktifKullanici = "";
+			new GirisEkrani().setVisible(true);
+			dispose();
+		});
+
+		int startX = 175;
+		int iconY = 250;
+		int labelY = 375;
+		int gap = 220;
+
+		// RAPORLAR
+		ImageIcon raporIcon = new ImageIcon("src/üys/raporlar.png");
+		Image raporImg = raporIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
+		JButton btnRapor = new JButton(new ImageIcon(raporImg));
+		btnRapor.setBounds(startX, iconY, 150, 120);
+		btnRapor.setContentAreaFilled(false);
+		btnRapor.setBorderPainted(false);
+		btnRapor.setFocusPainted(false);
+		contentPane.add(btnRapor);
+
+		JLabel lblRapor = new JLabel("Raporlar");
+		lblRapor.setBounds(startX, labelY, 150, 30);
+		lblRapor.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblRapor.setForeground(new Color(63, 81, 181));
+		lblRapor.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblRapor);
+
+		// MAKİNE GİRİŞİ
+		ImageIcon makineIcon = new ImageIcon("src/üys/makineGirisi.png");
+		Image makineImg = makineIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
+		JButton btnMakine = new JButton(new ImageIcon(makineImg));
+		btnMakine.setBounds(startX + gap, iconY, 150, 120);
+		btnMakine.setContentAreaFilled(false);
+		btnMakine.setBorderPainted(false);
+		btnMakine.setFocusPainted(false);
+		contentPane.add(btnMakine);
+
+		JLabel lblMakine = new JLabel("Makine Girişi");
+		lblMakine.setBounds(startX + gap, labelY, 150, 30);
+		lblMakine.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblMakine.setForeground(new Color(63, 81, 181));
+		lblMakine.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblMakine);
+
+		btnMakine.addActionListener(e -> {
+			new MakineGirisEkrani().setVisible(true);
+			dispose();
+		});
+
+		// PLANLAMA
+		ImageIcon planIcon = new ImageIcon("src/üys/planlama.png");
+		Image planImg = planIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
+		JButton btnPlan = new JButton(new ImageIcon(planImg));
+		btnPlan.setBounds(startX + gap * 2, iconY, 150, 120);
+		btnPlan.setContentAreaFilled(false);
+		btnPlan.setBorderPainted(false);
+		btnPlan.setFocusPainted(false);
+		contentPane.add(btnPlan);
+
+		JLabel lblPlan = new JLabel("Planlama");
+		lblPlan.setBounds(startX + gap * 2, labelY, 150, 30);
+		lblPlan.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblPlan.setForeground(new Color(63, 81, 181));
+		lblPlan.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblPlan);
+
+		// DURUŞ / KAYIP
+		ImageIcon durusIcon = new ImageIcon("src/üys/durusKayip.png");
+		Image durusImg = durusIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+
+		JButton btnDurus = new JButton(new ImageIcon(durusImg));
+		btnDurus.setBounds(startX + gap * 3, iconY, 150, 120);
+		btnDurus.setContentAreaFilled(false);
+		btnDurus.setBorderPainted(false);
+		btnDurus.setFocusPainted(false);
+		contentPane.add(btnDurus);
+
+		JLabel lblDurus = new JLabel("Duruş/Kayıp");
+		lblDurus.setBounds(startX + gap * 3, labelY, 150, 30);
+		lblDurus.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblDurus.setForeground(new Color(63, 81, 181));
+		lblDurus.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblDurus);
+
+		btnDurus.addActionListener(e -> {
+			new DuruşKayıp().setVisible(true);
+			dispose();
+		});
+	}
+
+	public static void main(String[] args) {
+		new OperatorMenu().setVisible(true);
 	}
 }
